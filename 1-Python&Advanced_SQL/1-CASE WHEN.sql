b5b74af03fe613119518883d6dc0a6652d5c41d7
Firas BAHRI <pro.bahri.firas@gmail.com>
2023-10-29 02:17:34 +0100

    DataSetsInsertion&DataPrepara°&AdvancedSQL1stSC

---
+++ 1-Python&Advanced_SQL/1-CASE WHEN.sql
@@ -0,0 +1,149 @@
+-- Active: 1686445526786@@127.0.0.1@3306@european_soccer_database
+USE european_soccer_database;
+
+-- Show tables
+SHOW TABLES;
+
+-- Select the team long name and team API id, Only include FC Schalke 04 and FC Bayern Munich
+SELECT 	
+	team_long_name,
+	team_api_id
+FROM teams_germany
+WHERE team_long_name IN ('FC Schalke 04', 'FC Bayern Munich');
+
+
+-- Identify the home team as Bayern Munich, Schalke 04, or neither & Group by the CASE statement alias
+SELECT 
+	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+        WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+GROUP BY home_team;
+
+
+-- Select team_long_name and team_api_id from team & Filter for team long name
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+WHERE team_long_name = 'Bologna';
+
+
+-- Select the season and date columns
+SELECT 
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 
+        AND home_goal > away_goal 
+        THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 
+        AND away_goal > home_goal 
+        THEN 'Bologna Win' 
+		END AS outcome
+FROM matches_italy;
+
+
+-- Select the season, date, home_goal, and away_goal columns
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
+
+
+-- Count games from the 2012/2013 season & Group by country name alias
+SELECT 
+	c.name AS country,
+	COUNT(CASE WHEN m.season = '2012/2013' 
+        	THEN m.id ELSE NULL END) AS matches_2012_2013
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+-- Count matches in each of the 3 seasons & Group by country name alias
+SELECT 
+	c.name AS country,
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+-- Sum the total records in each season where the home team won & Group by country name alias
+SELECT 
+	c.name AS country,
+	SUM(CASE WHEN m.season = '2012/2013' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2012_2013,
+ 	SUM(CASE WHEN m.season = '2013/2014' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2013_2014,
+	SUM(CASE WHEN m.season = '2014/2015' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+-- Count the home wins, away wins, and ties in each country
+SELECT 
+    c.name AS country,
+	COUNT(CASE WHEN m.home_goal > m.away_goal THEN m.id 
+        END) AS home_wins,
+	COUNT(CASE WHEN m.home_goal < m.away_goal THEN m.id 
+        END) AS away_wins,
+	COUNT(CASE WHEN m.home_goal = m.away_goal THEN m.id 
+        END) AS ties
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+-- Calculate the percentage of tied games in each season
+SELECT 
+	c.name AS country,
+	AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			END) AS ties_2013_2014,
+	AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			END) AS ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+-- Round the percentage of tied games to 2 decimal points
+SELECT 
+	c.name AS country,
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			 END),2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+
+
+
+
+
+
